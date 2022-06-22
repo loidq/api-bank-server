4579d0a9f1055981237d954d6503fdf9e7209b3e
sow-dev <sowz.dev@gmail.com>
2022-06-22 16:42:16 +0700

    create prj

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const logger = require('morgan')
+const mongoClient = require('mongoose')
+
+mongoClient
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('✅ Connected database from mongodb.'))
+	.catch((error) => console.error(`❌ Connect database is failed with error which is ${error}`))
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+	return res.status(201).json({
+		success: true,
+		message: 'Server is OK',
+	})
+})
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found')
+	err.status = 404
+	next(err)
+})
+
+app.use((err, req, res, next) => {
+	const status = err.status || 500
+	return res.status(status).json({
+		success: false,
+		error: {
+			message: err.message,
+		},
+	})
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server is listening on port ${port}`)).timeout = 10000
