1845b24de5366406963f8cc97a6e81f2af065d0b
sow-dev <sowz.dev@gmail.com>
2022-07-09 14:31:44 +0700

    sort limit page

--- controllers/wallet.js
+++ controllers/wallet.js
@@ -81,24 +81,50 @@ const GET_BALANCE = async (req, res, next) => {
 const GET_TRANSACTION = async (req, res, next) => {
 	const { _id } = req.bank
 
-	const list = await Transaction.find(
-		{
+	let page = req.query.page * 1 || 1
+	let limit = req.query.limit * 1 || 5
+
+	let skip = limit * (page - 1)
+	// const data = await Transaction.find(
+	// 	{
+	// 		banks: _id,
+	// 		status: true,
+	// 	},
+	// 	{ _id: 0, banks: 0, owner: 0, createdAt: 0, updatedAt: 0, __v: 0, status: 0, serviceId: 0 }
+	// )
+	// 	.limit(limit)
+	// 	.skip(skip)
+	// 	.sort({
+	// 		time: 1,
+	// 	})
+
+	// let total = await Transaction.countDocuments({
+	// 	banks: _id,
+	// 	status: true,
+	// })
+
+	const result = await Promise.allSettled([
+		Transaction.find(
+			{
+				banks: _id,
+				status: true,
+			},
+			{ io: 1, transId: 1, partnerId: 1, partnerName: 1, amount: 1, postBalance: 1, time: 1, comment: 1, _id: 0 }
+		)
+			.limit(limit)
+			.skip(skip)
+			.sort({
+				time: 1,
+			}),
+		Transaction.countDocuments({
 			banks: _id,
 			status: true,
-		},
-		{ _id: 0, banks: 0, owner: 0, createdAt: 0, updatedAt: 0, __v: 0, status: 0, serviceId: 0 }
-	).sort({
-		time: -1,
-	})
+		}),
+	])
+	const data = result[0].status === 'fulfilled' ? result[0].value : []
+	const total = result[1].status === 'fulfilled' ? result[1].value : 0
 
-	let total = await Transaction.countDocuments({
-		banks: _id,
-		status: true,
-	})
-
-	//const accounts = await await Bank.find({ owner: userID, bank },	{ username: 1, status: 1, createdAt: 1, bank: 1 }).limit(pageSize).skip(pageSize * (pageIndex - 1))
-
-	return res.status(200).json({ success: true, data: list, total })
+	return res.status(200).json({ success: true, data, total })
 }
 
 module.exports = { createImei, SEND_OTP_MOMO, CONFIRM_OTP_MOMO, CHECK_MONEY, GET_BALANCE, GET_TRANSACTION }
